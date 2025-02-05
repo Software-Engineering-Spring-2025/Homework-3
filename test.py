6ee16abc549356e654ae91dddb69298f6d45a3bb
jillahmad17 <133699478+jillahmad17@users.noreply.github.com>
2025-02-04 21:36:58 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ test for negative numbers
 from hw2_debugging import merge_sort  # Assuming your code is in my_module.py
 
 def test_case3():
-'''
-test for negative numbers
-'''
+    '''
+    test for negative numbers
+    '''
     random_array = [-1,-2,-3,-4,-5,-1]
     sorted_array = merge_sort(random_array)
     assert sorted_array == [-5,-4,-3,-2,-1,-1], "works no probs"
