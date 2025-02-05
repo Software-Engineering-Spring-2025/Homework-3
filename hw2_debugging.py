dcd042fb8e235837cfbf4fa07f9e69a7d191cc85
jillahmad17 <jillahmad17@gmail.com>
2025-02-04 21:15:30 -0500

    creating test case

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -4,7 +4,7 @@ import rand
 
 def merge_sort(array):
     """Function for merge sort."""
-    if len(array) == 1:
+    if len(array) <= 1:
         return array
 
     half = len(array)//2
@@ -16,20 +16,31 @@ def recombine(left_arr, right_arr):
     left_index = 0
     right_index = 0
     merge_arr = [None] * (len(left_arr) + len(right_arr))
+    merge_ind = 0
+
     while left_index < len(left_arr) and right_index < len(right_arr):
+
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
-            merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
+            merge_arr[merge_ind] = left_arr[left_index]
             left_index += 1
-            merge_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+        else:
+            merge_arr[merge_ind] = right_arr[right_index]
+            right_index += 1
+        merge_ind += 1
 
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    for index, value in enumerate(right_arr[right_index:]):
+        merge_arr[merge_ind] = value
+        merge_ind += 1
 
+    for index, value in enumerate(left_arr[left_index:]):
+        merge_arr[merge_ind] = value
+        merge_ind += 1
+
+    for index, value in enumerate(left_arr):
+        left_arr[index] = merge_arr[index]
+    for index, value in enumerate(right_arr):
+        right_arr[index] = merge_arr[len(left_arr)]
     return merge_arr
 
 arr = rand.random_array([None] * 20)
---
+++ test.py
@@ -0,0 +1,10 @@
+from hw2_debugging import merge_sort  # Assuming your code is in my_module.py
+import rand
+
+def test_case3():
+    '''
+    test for negative numbers
+    '''
+    random_array = [-1,-2,-3,-4,-5,-1]
+    sorted_array = merge_sort(random_array)
+    assert sorted_array == [-5,-4,-3,-2,-1,-1], "works no probs"
