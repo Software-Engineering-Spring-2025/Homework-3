22289106b7e056332e7728d7fada3824bd06ddf3
SrinivasVasudevan <srinivasvasudevan2000@gmail.com>
2025-02-04 21:07:02 -0500

    test files fixes

--- hw2_test.py
+++ hw2_test.py
@@ -2,7 +2,6 @@
 3 test cases to test various scenarios that could possibly come up during merge sort
 '''
 from hw2_debugging import merge_sort
-import rand
 def test_case1():
     '''
     test for empty list edge case
@@ -10,18 +9,3 @@ def test_case1():
     random_array = []
     sorted_array = merge_sort(random_array)
     assert sorted_array == [], "works no probs"
-def test_case2():
-    '''
-    test for reverse sorted list
-    '''
-    random_array = [9,8,7,6,5,4,3,2,1]
-    sorted_array = merge_sort(random_array)
-    assert sorted_array == [1,2,3,4,5,6,7,8,9], "works no probs"
-def test_case3():
-    '''
-    test for negative numbers
-    '''
-    random_array = [-1,-2,-3,-4,-5,-1]
-    sorted_array = merge_sort(random_array)
-    assert sorted_array == [-5,-4,-3,-2,-1,-1], "works no probs"
-
