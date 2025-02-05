e41fe6e53bbfe962de416d7903fc066577ed6b5f
Saamyar1 <125711717+Saamyar1@users.noreply.github.com>
2025-02-04 21:15:39 -0500

    test case to make sure all our tasks are correct

---
+++ test.py
@@ -0,0 +1,9 @@
+from hw2_debugging import merge_sort
+
+def test_case2():
+    '''
+    test for reverse sorted list
+    '''
+    random_array = [9,8,7,6,5,4,3,2,1]
+    sorted_array = merge_sort(random_array)
+    assert sorted_array == [1,2,3,4,5,6,7,8,9], "works no probs"
